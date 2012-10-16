db6f22e829886d3f34e7863d110d0437b6a9f78e
Aaron Lifton <theflubba@gmail.com>
2012-10-16 17:41:24 -0400

    removed duplicate code, added initial readme

--- goweb.go
+++ goweb.go
@@ -9,8 +9,6 @@ import (
 	"net/http"
 )
 
-const dbParams string = `host=ec2-54-243-239-221.compute-1.amazonaws.com user=kwcwqwdgfelhrs port=5432 password=PASSWORD_HERE dbname=d62335du1mgsdc sslmode=require`
-
 func main() {
 	http.HandleFunc("/", view.MakeHandler(view.HomeHandler))
 	http.HandleFunc("/getjson", view.JSONHandler)
